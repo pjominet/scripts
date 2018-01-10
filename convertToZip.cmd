a7e7b22adfa82a186ccd1a2a2bc8bde8ef53a79d
Patrick <jompa010@gmail.com>
2018-01-10 01:46:04 +0100

    fixes

--- convertToZip.cmd
+++ convertToZip.cmd
@@ -9,8 +9,8 @@ SETLOCAL ENABLEEXTENSIONS
 SET PATH=%PATH%;D:\Program Files\7-Zip\
 SET me=%~n0
 SET archive=%~1
-FOR /F %%i IN (%archive%) DO SET extracted=%%~ni
-ECHO Archive name %extracted%
+FOR %%i IN (%archive%) DO SET extracted=%%~ni
+FOR %%i IN (%archive%) DO SET extension=%%~xi
 
 :: main
 IF "%~1"=="--help" (
@@ -19,8 +19,8 @@ IF "%~1"=="--help" (
 )
 IF NOT [%~1]==[] (
 	IF EXIST "%archive%" (
-		ECHO Now converting: %a_name% to zip format...
-		ECHO Extracting...
+		ECHO Now converting: %archive% to zip format...
+		ECHO Extracting %extension%-file...
 		7z x %archive% -o%extracted% -aoa
 		ECHO Recompressing into zip...
 		7z a -tzip %extracted%.zip .\%extracted%\* -mx5
